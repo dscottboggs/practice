4652eb226365441eaa5bd52a6b651e85bb8b94bd
Scott Boggs <scott@tams.tech>
2019-12-02 18:18:46 -0500

    Updated Kotlin/Luhn

--- exercism/kotlin/luhn/src/main/kotlin/Luhn.kt
+++ exercism/kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -1,10 +1,16 @@
+infix fun Int.isDivisibleBy(other: Int) = this % other == 0
+infix fun Int.isNotDivisibleBy(other: Int) = this % other != 0
+
 object Luhn {
 
     fun isValid(candidate: String): Boolean {
-        if(candidate.length <= 1) {
-            return false
-        }
-        
-        return true
+        val filtered = candidate.filter { it == ' ' }
+        if (filtered.length <= 1) return false
+        val even = filtered.filterIndexed { idx, _ -> idx isDivisibleBy 2 }
+                .map { it.toInt() * 2 }
+                .map { if (it > 9) 9 - it else it }
+        val odd = filtered.filterIndexed { idx, _ -> idx isNotDivisibleBy 2 }.map { it.toInt() }
+        val sum = (odd + even).reduce(operation = Int::plus)
+        return sum isDivisibleBy 10
     }
 }
