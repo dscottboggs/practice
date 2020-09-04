9c387e957355cd0269b1a50dadadee5c49ca0527
D. Scott Boggs <scott@tams.tech>
2020-09-04 13:49:19 -0400

    use operator== for rust sublist instead of custom equality check function

--- exercism/rust/sublist/src/lib.rs
+++ exercism/rust/sublist/src/lib.rs
@@ -8,13 +8,6 @@ pub enum Comparison {
     Unequal,
 }
 
-fn check_equal<T: PartialEq>(one: &[T], two: &[T]) -> Comparison {
-    if one.iter().enumerate().all(|(i, e)| &two[i] == e) {
-        Comparison::Equal
-    } else {
-        Comparison::Unequal
-    }
-}
 fn is_sublist<T: PartialEq>(one: &[T], two: &[T]) -> bool {
     two.windows(one.len()).any(|window| window == one)
 }
@@ -25,7 +18,7 @@ pub fn sublist<T: PartialEq>(one: &[T], two: &[T]) -> Comparison {
         (0, _) => Comparison::Sublist,
         (_, 0) => Comparison::Superlist,
         (len_one, len_two) => match len_one.cmp(&len_two) {
-            Equal => check_equal(one, two),
+            Equal if one == two => Comparison::Equal,
             Less if is_sublist(one, two) => Comparison::Sublist,
             Greater if is_sublist(two, one) => Comparison::Superlist,
             _ => Comparison::Unequal,
